6c7efd7cdb07bfd2e2970ff12acedcc05ae227f9
jasonbyers <jasondene@hotmail.com>
2024-06-23 08:59:47 -0500

    testing

--- monitoring.py
+++ monitoring.py
@@ -6,6 +6,6 @@ ser = serial.Serial(‘/dev/ttyUSB0’)
 while True: data = []
 for index in range(0,10): datum = ser.read() data.append(datum)
 pmtwofive = int.from_bytes(b’’.join(data[2:4]), byteorder=’little’) / 10
-aio.send(‘kingswoodtwofive’, pmtwofive)
+aio.send(‘minneapoliswofive’, pmtwofive)
 pmten = int.from_bytes(b’’.join(data[4:6]), byteorder=’little’) / 10
-aio.send(‘kingswoodten’, pmten) time.sleep(10)
+aio.send(‘minneapolisten’, pmten) time.sleep(10)
